a8e5c7bf0680251d4de9a949799e125b4c1ebec1
Anykey84 <Anykey84@gmail.com>
2016-09-28 16:22:44 +0400

    коммит

--- development/js/template.jsx
+++ development/js/template.jsx
@@ -1,16 +1,24 @@
 const Header = React.createClass({
     render: function() {
         return (
-            <header className="flex-between-center">
-                <div className="logo">
-                    <div>Escimo</div>
-                    <div className="flex-between"><p>ask</p><p>me</p><p>more</p></div>
+            <header className="flex-center">
+                <div className="container flex-between-center">
+                    <div className="logo">
+                        <div>Escimo</div>
+                        <div className="flex-between">
+                            <p>ask</p>
+                            <p>me</p>
+                            <p>more</p>
+                        </div>
+                    </div>
+                    <form className="search-or-ask" name="search-or-ask">
+                        <input type="text" name="ask" placeholder="search or ask"/>
+                        <input type="submit" className="btn" value="Ask a Question"/>
+                    </form>
+                    <div className="login-button">
+                        <a href="#">Login</a>
+                    </div>
                 </div>
-                <form className="search-or-ask" name="search-or-ask">
-                  <input type="text" name="ask" placeholder="search or ask" />
-                  <input type="submit" className="btn" value="Ask a Question" />
-                </form>
-                <div className="login-button"><a href="#">Login</a></div>
             </header>
         )
     }
