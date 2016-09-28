aa6e6fad55da1b1a2ab8edc739f73953df434c99
Anykey84 <Anykey84@gmail.com>
2016-09-28 15:26:00 +0400

    вывел header

--- dist/js/template.js
+++ dist/js/template.js
@@ -2,7 +2,7 @@
 
 var Header = React.createClass({ displayName: "Header",
     render: function render() {
-        return React.createElement("header", { className: "flex-between" }, React.createElement("div", { className: "logo" }, React.createElement("div", null, "Escimo"), React.createElement("div", { className: "flex-between" }, React.createElement("p", null, "ask"), React.createElement("p", null, "me"), React.createElement("p", null, "more"))));
+        return React.createElement("header", { className: "flex-between-center" }, React.createElement("div", { className: "logo" }, React.createElement("div", null, "Escimo"), React.createElement("div", { className: "flex-between" }, React.createElement("p", null, "ask"), React.createElement("p", null, "me"), React.createElement("p", null, "more"))), React.createElement("form", { className: "search-or-ask", name: "search-or-ask" }, React.createElement("input", { type: "text", name: "ask", placeholder: "search or ask" }), React.createElement("input", { type: "submit", className: "btn", value: "Ask a Question" })), React.createElement("div", { className: "login-button" }, React.createElement("a", { href: "#" }, "Login")));
     }
 });
 
